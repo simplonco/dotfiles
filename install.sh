e75ee10bc7e61833ddf51fc6857679853c970f23
Yvan Sraka <ysraka@simplon.co>
2016-11-04 20:05:16 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 #! /bin/sh
 
+## 1
 apt-get update
 apt-get install xorg i3 suckless-tools wireless-tools network-manager firmware-iwlwifi
+
+# 2
 apt-get install sudo git wget curl screen vim zsh chromium flashplugin-nonfree
 
-# IMPORTANT: please note that you might override existing
-# configuration files in the current working directory!
+# 3
 wget -O .screenrc     http://git.grml.org/f/grml-etc-core/etc/grml/screenrc_generic
 wget -O .vimrc        http://git.grml.org/f/grml-etc-core/etc/vim/vimrc
 wget -O .zshrc        http://git.grml.org/f/grml-etc-core/etc/zsh/zshrc
